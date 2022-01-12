16c166d23b8cdb6a24efd96285f0385ab6d4aba4
linfeng <18574786523@163.com>
2022-01-12 23:07:56 +0800

    简单的完成了项目的访问功能

--- src/main/java/com/lin/HelloServlet.java
+++ src/main/java/com/lin/HelloServlet.java
@@ -16,5 +16,7 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = "我的简易框架";
+        req.setAttribute("name",name);
+        req.getRequestDispatcher("WEB-INFO/jsp/hello.jsp").forward(req,resp);
     }
 }
